d0062f0610a5236af8883ee0536b2847243e7870
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-08-26 17:58:29 -0300

    feat: Iniciando Pokedex em View Code com  MVC.

---
+++ PokedexMVC/PokedexMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokedexMVC
+//
+//  Created by user on 26/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
